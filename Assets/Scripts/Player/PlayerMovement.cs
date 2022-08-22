7ef2c1c2dabf4453a218213bc0b553f7b101276c
Sobhan <s.kevishahi@gmail.com>
2022-08-21 20:44:51 -0400

    Created Player Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    private PlayerInputActions playerInputActions;
+    private InputAction movement;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        movement = playerInputActions.Player.Movement;
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = speed * movement.ReadValue<Vector2>();
+    }
+}
