2717b8b481af79d3f563e38f42ea4095db19dd0b
Sobhan <s.kevishahi@gmail.com>
2022-08-22 20:30:13 -0400

    Swapping Changes Virtual Camera Follow

--- Assets/Scripts/Player/CharacterSwapper.cs
+++ Assets/Scripts/Player/CharacterSwapper.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class CharacterSwapper : MonoBehaviour
 {
@@ -9,16 +10,20 @@ public class CharacterSwapper : MonoBehaviour
     [SerializeField] GameObject dogCharacter;
 
     private PlayerInputActions playerInputActions;
-    private PlayerMovement playerMovement;
-    private PlayerMovement petMovement;
+    private PlayerMovement humanMovement;
+    private PlayerMovement dogMovement;
     private bool isHumanInControl;
+
+    private CinemachineVirtualCamera virtualCamera;
 
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
 
-        playerMovement = humanCharacter.GetComponent<PlayerMovement>();
-        petMovement = dogCharacter.GetComponent<PlayerMovement>();
+        humanMovement = humanCharacter.GetComponent<PlayerMovement>();
+        dogMovement = dogCharacter.GetComponent<PlayerMovement>();
+
+        virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
     }
 
     private void OnEnable()
@@ -36,15 +41,24 @@ public class CharacterSwapper : MonoBehaviour
     {
         isHumanInControl = true;
 
-        playerMovement.enabled = isHumanInControl;
-        petMovement.enabled = !isHumanInControl;
+        humanMovement.enabled = isHumanInControl;
+        dogMovement.enabled = !isHumanInControl;
     }
 
     private void SwapCharacter()
     {
         isHumanInControl = !isHumanInControl;
 
-        playerMovement.enabled = isHumanInControl;
-        petMovement.enabled = !isHumanInControl;
+        humanMovement.enabled = isHumanInControl;
+        dogMovement.enabled = !isHumanInControl;
+
+        if (isHumanInControl)
+        {
+            virtualCamera.Follow = humanCharacter.transform;
+        }
+        else
+        {
+            virtualCamera.Follow = dogCharacter.transform;
+        }
     }
 }
